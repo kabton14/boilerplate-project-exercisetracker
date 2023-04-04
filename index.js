8617909e95555446c2776b2c87b5f1596e7ff65b
Kevin Beaton <kabton14@gmail.com>
2023-04-03 20:13:27 -0500

    Connect to data source

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const express = require('express')
 const app = express()
-const mongoose = require('mongoose')
 const cors = require('cors')
 require('dotenv').config()
+
+const mongoose = require('mongoose')
+mongoose.set("strictQuery", true)
+mongoose.connect(process.env['MONGO_URI'], { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
-
-
-
-
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
